611cdc69497dc5b45d744c577f9c2b745d186392
Alex808r <fromxlam@gmail.com>
2021-09-13 20:16:04 +0300

    Add day number date

---
+++ ruby_basics_2/5_day_number_date.rb
@@ -0,0 +1,42 @@
+# Заданы три числа, которые обозначают число, месяц, год (запрашиваем у пользователя).
+# Найти порядковый номер даты, начиная отсчет с начала года. Учесть, что год может быть високосным.
+# (Запрещено использовать встроенные в ruby методы для этого вроде Date#yday или Date#leap?)
+# Алгоритм опредления високосного года: www.adm.yar.ru
+
+days_on_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+puts "Количество дней по месяцам: #{days_on_month}"
+
+print "Ввидеите число: "
+day = Integer(gets) # day = gets.to_i
+
+print "Введите месяц: "
+month = Integer(gets)
+
+print "Введите год: "
+year = Integer(gets)
+
+# Проверяем, является ли год високосным.
+leap_year = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+days_on_month[1] = 29 if leap_year == true
+
+abort "Выбранной даты не существует" if day > days_on_month[month - 1] || month > days_on_month.size
+
+# без тернарного оператора
+# if month == 1
+#   day
+# else
+#   months[0...(month - 1)].each do |i|  #  months[0..(month - 2)].each do |i|
+#     day += i
+#   end
+# end
+
+unless month == 1
+  # days_on_month[0...(month - 1)].each do |i|  #  months[0..(month - 2)].each do |i|
+  #   day += i
+  # end
+
+  #аналогичная запись
+  result = days_on_month[0...(month - 1)].reduce(:+) + day
+end
+
+puts "Порядковый номер даты #{result}"
