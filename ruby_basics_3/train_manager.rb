6ec4f55add5e1f546b9e173b6e62dc01bc31ed2e
Alex808r <fromxlam@gmail.com>
2021-09-15 15:05:58 +0300

    Add commands to file train manager

--- ruby_basics_3/train_manager.rb
+++ ruby_basics_3/train_manager.rb
@@ -19,6 +19,9 @@ ro = Route.new(kazan, kirov)
 ro.add_station(novgorod)
 ro.add_station(moscow)
 
+# Проверим количество станций на маршруте. Должно быть 4.
+ro.stations
+
 # Поезд принимает маршурт
 train_1.take_route(ro)
 train_2.take_route(ro)
@@ -35,6 +38,10 @@ train_1.move_forward
 # Проверим сколько поездов на станции Казань. Должен быть 1.
 kazan.list_train
 
+# Проверим где стоит поезд 1. Должен быть в Новгороде
+train_1.current_station
+novgorod.list_train
+
 # Проверим где стоит поезд 2. Должен быть на станции Казань.
 train_2.current_station
 
@@ -44,4 +51,20 @@ train_2.previous_station
 # Отправим поезд 2 на предыдущую станцию. Должны получить nil так как поезд на первой станции.
 train_2.move_bakward
 
+# Проверим сколько поездов каждого типа на станции Казань. cargo = 1, passanger = 0
+kazan.train_type_count('cargo')
+kazan.train_type_count('passenger')
 
+# Проверим наличие пассажироского поезда на станции Новгород.
+novgorod.train_type_count('passenger')
+novgorod.train_type('passenger')
+
+# Отправим поезд 2 на конечную станцию
+train_2.move_forward
+train_2.move_forward
+train_2.move_forward
+train_2.current_station
+
+# Проверим предыдущую и следующую. nil следующая, Moscow предыдущая.
+train_2.next_station
+train_2.previous_station
