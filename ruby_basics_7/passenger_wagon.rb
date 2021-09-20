ce4b03435ef92dbf676da2b9f743fac047ff0fc3
Alex808r <fromxlam@gmail.com>
2021-09-20 17:59:05 +0300

    Init commit Ruby basics 8

--- ruby_basics_7/passenger_wagon.rb
+++ ruby_basics_7/passenger_wagon.rb
@@ -2,11 +2,29 @@ require_relative "wagon"
 
 class PassengerWagon < Wagon
 
-  attr_reader :type
+  attr_reader :type, :number_of_seats, :occupied_place
 
-  def initialize(number_wagon)
+  def initialize(number_wagon, number_of_seats)
     @type = 'passenger'
+    @number_of_seats = number_of_seats
+    @occupied_place = 0
     super(number_wagon)
   end
 
+  def take_the_place
+    @occupied_place += 1
+  end
+
+  def free_place
+    @number_of_seats.to_i - @occupied_place.to_i
+  end
+
 end
+
+# wagon = PassengerWagon.new('12','45')
+# p wagon.number_of_seats
+# p wagon.take_the_place
+# p wagon.take_the_place
+# p wagon.take_the_place
+# p wagon.take_the_place
+# p wagon.free_place
